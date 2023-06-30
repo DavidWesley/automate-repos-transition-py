b2535295768c5d163124e8c6c539a7537e0e1751
DavidWesley <david.wesley755@outlook.com>
2023-06-30 20:10:45 -0300

    refactor: Add comments and docstrings to the code
    I added a class docstring to provide an overall description of the class. I also added comments to explain the purpose of each color constant

--- src/colors.py
+++ src/colors.py
@@ -1,10 +1,11 @@
 class ANSIColors:
-	PURPLE = '\033[95m'
-	BLUE = '\033[94m'
-	GREEN = '\033[90m'
-	YELLOW = '\033[93m'
-	RED = '\033[91m'
-	RESET = '\033[0m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
+    """A class representing ANSI escape codes for text colors."""
 
+    PURPLE = '\033[95m'  # Purple color
+    BLUE = '\033[94m'  # Blue color
+    GREEN = '\033[90m'  # Green color
+    YELLOW = '\033[93m'  # Yellow color
+    RED = '\033[91m'  # Red color
+    RESET = '\033[0m'  # Reset color
+    BOLD = '\033[1m'  # Bold text
+    UNDERLINE = '\033[4m'  # Underline text
