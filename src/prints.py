d7f7e8f3f380c65bee2011d42ab17962371f6a1d
DavidWesley <david.wesley755@outlook.com>
2023-06-30 20:16:36 -0300

    refactor: Removed unnecessary verifications

--- src/prints.py
+++ src/prints.py
@@ -2,37 +2,25 @@ from logger import logger
 from colors import ANSIColors
 
 
-def print_info(message: str, end: str | None = None) -> None:
-	if end is None:
-		end = "\n"
-
+def print_info(message: str, end: str = "\n") -> None:
 	logger.info(message)
 	print(f"{ANSIColors.BLUE}{ANSIColors.BOLD}INFO:{ANSIColors.BOLD}", end="\t")
 	print(f"{ANSIColors.BLUE}{message}\t{ANSIColors.RESET}", end=end)
 
 
-def print_err(message: str, end: str | None = None) -> None:
-	if end is None:
-		end = "\n"
-
+def print_err(message: str, end: str = "\n") -> None:
 	logger.error(message)
 	print(f"{ANSIColors.RED}{ANSIColors.BOLD}ERROR:{ANSIColors.BOLD}", end="\t")
 	print(f"{ANSIColors.RED}{message}\t{ANSIColors.RESET}", end=end)
 
 
-def print_warning(message: str, end: str | None = None) -> None:
-	if end is None:
-		end = "\n"
-
+def print_warning(message: str, end: str = "\n") -> None:
 	logger.warning(message)
 	print(f"{ANSIColors.YELLOW}{ANSIColors.BOLD}WARNING:{ANSIColors.BOLD}", end="\t")
 	print(f"{ANSIColors.YELLOW}{message}\t{ANSIColors.RESET}", end=end)
 
 
-def print_header(message: str, end: str | None = None) -> None:
-	if end is None:
-		end = "\n"
-
+def print_header(message: str, end: str = "\n") -> None:
 	print(f"{ANSIColors.UNDERLINE}{ANSIColors.BOLD}{message}\t{ANSIColors.RESET}", end=end)
 
 def print_debug(message: str) -> None:
